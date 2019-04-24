0ebb0e1da9a2cc9375e6c8c1b6d3550c2de5103c
PHironaka <peter@peterhironaka.com>
2019-04-23 21:45:17 -0700

    news placeholder posts

---
+++ src/components/arrow.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Redress = styled.svg `
+   padding: 0 1em;
+   max-width: 58px;
+   .cls-1{fill:none;stroke:#000;stroke-miterlimit:10;stroke-width:2px}
+`
+
+const SvgComponent = props => (
+        <Redress viewBox="0 0 26.37 19.41" xmlns="http://www.w3.org/2000/svg">
+            <g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M15.96.71l9 9-9 9M24.96 9.71H0"></path></g></g>
+        </Redress>
+)
+
+export default SvgComponent
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,15 +1,45 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { Component } from "react"
 import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Arrow from '../components/arrow'
+import FadeIn from 'react-fade-in';
+import Img from 'gatsby-image'
 
 const NewsPage = styled.div`
   padding: 0 2em;
   margin-top: -51px;
-
 `
-const SecondPage = () => (
+const NewsContainer = styled.div`
+  border-top:1px solid;
+  margin-top:2em;
+  padding:1em 0;
+`
+const Post = styled.div`
+  display:grid;
+  grid-template-columns:1fr 1fr;
+  grid-gap: 2em;
+  margin: 1em 0 2em;
+
+  @media screen and (max-width: 800px) {
+      display:block;
+  }
+
+  h2 {
+    font-size:1.4em;
+    margin:0 0 1em;
+  }
+`
+const ReadArticle = styled.a`
+  margin:2em 0;
+  display:block;
+`
+
+class News extends Component {
+  render() {
+    const data = this.props.data
+
+    return (
   <Layout>
     <SEO title="Page two" />
     <NewsPage>
@@ -27,8 +57,53 @@ const SecondPage = () => (
                }} />
       <h1>News</h1>
       <p>The latest news, updates, and insights from RedDress. </p>
+      <NewsContainer>
+
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <FadeIn transitionDuration={500}>
+          <Post>
+            <div>
+              <h2>{node.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: node.content }} />
+              <ReadArticle href="https://google.com" target="_blank">Read More <Arrow /> </ReadArticle>
+            </div>
+            <Img fluid={node.featured_media.localFile.childImageSharp.fluid} alt={node.id}/>
+
+          </Post>
+        </FadeIn>
+      ))}
+      </NewsContainer>
     </NewsPage>
   </Layout>
-)
+    )
+  }
+}
 
-export default SecondPage
+
+export default News
+
+export const NewsQuery = graphql`
+query {
+  allWordpressPost   {
+        edges {
+        node {
+          id
+          title
+          excerpt
+          content
+          slug
+          date(formatString: "MMMM DD, YYYY")
+          featured_media {
+            localFile {
+              childImageSharp {
+              fluid(maxWidth: 1000) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+            }
+          }
+        }
+      }
+    }
+}
+`
