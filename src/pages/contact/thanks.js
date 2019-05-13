965128d823b2ceecbc18361ee733503aad0b4eb0
PHironaka <peter@peterhironaka.com>
2019-05-13 16:22:24 -0700

    update email confirm

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Layout from '../../components/layout'
+import styled from 'styled-components'
+
+const EmailConfirm = styled.div`
+  max-width:1200px;
+  padding: 0 2em;
+  margin: 0 auto;
+`
 
 export default () => (
   <Layout>
-        <section className="section">
+        <EmailConfirm>
           <div className="container">
             <div className="content">
                 <h1>Thank you!</h1>
                 <p>This is a custom thank you page for form submissions</p>
             </div>
           </div>
-          </section>
+          </EmailConfirm>
   </Layout>
 );
